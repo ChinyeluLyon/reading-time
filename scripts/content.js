fa410e6716a635d7e7972437e2f04cf981e640c0
Chinyelu Lyon <chinyelulyon@gmail.com>
2023-06-12 22:21:57 +0100

    playing around with things

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,24 @@
-const article = document.querySelector("article");
+window.addEventListener("load", function () {
+  console.log("loaded page");
+  const body = document.querySelector("body");
+  // `document.querySelector` may return null if the selector doesn't match anything.
+  if (body) {
+    const text = body.textContent;
+    const wordMatchRegExp = /[^\s]+/g; // Regular expression
+    const words = text.matchAll(wordMatchRegExp);
+    // matchAll returns an iterator, convert to array to get word count
+    const wordCount = [...words].length;
+    const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("p");
+    // Use the same styling as the publish information in an body's header
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`;
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}
+    // Support for API reference docs
+    const heading = body.querySelector("h1");
+    // Support for body docs with date
+    const date = body.querySelector("time")?.parentNode;
+    // console.log(text);
+    (date ?? heading).insertAdjacentElement("afterend", badge);
+  }
+});
